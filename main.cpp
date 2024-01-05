422c98ffe601f8ab69631f26cb81f0811329d508
amiralitw9 <82338163+amiralitw9@users.noreply.github.com>
2024-01-05 15:09:50 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+using namespace std ;
+
+void ball_green(){} //ball number 1
+void ball_yellow(){}//ball number 2
+void ball_red(){}//ball number 3
+void ball_purple(){}//ball number 4
+void ball_blue(){}//ball number 5
+void ball_red_yellow(){}//ball number 6
+void ball_red_blue(){}//ball number 7
+void ball_red_purple(){}//ball number 8
+void ball_red_green(){}//ball number 9
+void ball_blue_green(){}//ball number 10
+void ball_blue_yellow(){}//ball number 11
+void ball_blue_purple(){}//ball number 12
+void ball_green_yellow(){}//ball number 13
+void ball_green_purple(){}//ball number 14
+void ball_purple_yellow(){}//ball number 15
+void ball_black() {}//ball number 16
+void ball_lock_green(){}//ball number 17
+void ball_lock_red(){}//ball number 18
+void ball_lock_yellow(){}//ball number 19
+void ball_lock_purple(){}//ball number 20
+void ball_lock_blue(){}//ball number 21
